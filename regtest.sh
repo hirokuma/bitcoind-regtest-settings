7e7708c63363978872c066de0bc804c3e5c6ea48
hirokuma <hiro9ma@gmail.com>
2026-05-02 14:44:53 +0900

    add regtest.sh

---
+++ regtest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CLI='bitcoin-cli -regtest'
+
+if [ -f ".env" ]; then
+    source .env
+fi
+
+if [[ $# == 0 ]]; then
+  echo "Help:"
+  echo "    $0 start: start bitcoin node"
+  echo "    $0 wallet: create wallet"
+  echo "    $0 generate <NUM>: generatetoaddress"
+  exit 1
+elif [[ $# == 1 ]] && [[ "$1" == "wallet" ]]; then
+  $CLI loadwallet $WALLET_NAME
+elif [[ $# == 1 ]] && [[ "$1" == "generate" ]]; then
+  $CLI generatetoaddress 1 `$CLI getnewaddress`
+elif [[ $# == 2 ]] && [[ "$1" == "generate" ]]; then
+  $CLI generatetoaddress $2 `$CLI getnewaddress`
+else
+  $CLI $@
+fi
